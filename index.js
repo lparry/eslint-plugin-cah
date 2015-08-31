9f0d25d9bb51fc333600a73e16881edecc079eb6
Ben Youngblood <bx.youngblood@gmail.com>
2015-08-31 10:15:16 -0700

    Add no-promise-undef

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 
 module.exports = {
   rules : {
-    'no-const-reassign' : require('./rules/no-const-reassign')
+    'no-const-reassign' : require('./rules/no-const-reassign'),
+    'no-undef-promise' : require('./rules/no-undef-promise')
   },
   ruleConfig : {
-    'no-const-reassign' : 0
+    'no-const-reassign' : 0,
+    'no-undef-promise' : 0
   }
 };
